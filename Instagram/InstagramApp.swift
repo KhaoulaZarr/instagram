2e409ba857ed71e3b0f84ecc2ca73b51df405a7a
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-11-26 10:23:44 +0100

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Khawla Zarrami on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

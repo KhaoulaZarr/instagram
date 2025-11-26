b35b1d4d0a52a0ea3baaddd30a3df3ce39eb4638
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-11-26 14:00:55 +0100

    Implement feed view

---
+++ Instagram/Core/Feed/FeedView.swift
@@ -0,0 +1,45 @@
+//
+//  FeedView.swift
+//  Instagram
+//
+//  Created by Khawla Zarrami on 26/11/2025.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { post in
+                        FeedCell()
+                        
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image("instagram_logo_black")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 120, height: 32)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    
+                        Image(systemName: "paperplane")
+                        .imageScale(.large)
+                    
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    FeedView()
+}

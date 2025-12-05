733003021c9709e8a8285d2ded8565b2b0ded0d5
Khawla Zarrami <zarramikhaoula@gmail.com>
2025-12-05 14:24:14 +0100

    Implement post data model

--- Instagram/Core/Feed/FeedCell.swift
+++ Instagram/Core/Feed/FeedCell.swift
@@ -8,19 +8,24 @@
 import SwiftUI
 
 struct FeedCell: View {
+    let post: Post
+    
     var body: some View {
         VStack {
             // image + username
             HStack {
-                Image("batman-2")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                    .clipShape(Circle())
+                if let user = post.user {
+                    Image(user.profileImageUrl ?? "")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipShape(Circle())
+                    
+                    Text(user.username)
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
                 
-                Text("batman")
-                    .font(.footnote)
-                    .fontWeight(.semibold)
                 
                 Spacer()
             }
@@ -28,7 +33,7 @@ struct FeedCell: View {
             
             // post image
     
-            Image("batman-2")
+            Image(post.imageUrl)
                 .resizable()
                 .scaledToFill()
                 .frame(height: 400)
@@ -64,7 +69,7 @@ struct FeedCell: View {
             
             
             // likes label
-            Text("23 likes")
+            Text("\(post.likes) likes")
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -73,8 +78,8 @@ struct FeedCell: View {
             
             // caption label
             HStack {
-                Text("batman ").fontWeight(.semibold) +
-                Text("This is some test caption for now" )
+                Text("\(post.user?.username ?? "") ").fontWeight(.semibold) +
+                Text(post.caption )
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .font(.footnote)
@@ -93,5 +98,5 @@ struct FeedCell: View {
 }
 
 #Preview {
-    FeedCell()
+    FeedCell(post: Post.MOCK_POSTS[0])
 }
